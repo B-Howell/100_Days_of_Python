723a9869c6e6a8b425f5a45aa93365e7516f51c0
B-Howell <bretthowell10@gmail.com>
2024-04-02 16:55:38 -0400

    started day 18

---
+++ day-18/start/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
